2314a53d7a42116764b7bef5ae7bfa4835ce4ab9
claymor_wan <claymorw@gmail.com>
2024-07-28 16:18:18 +0200

    Actual webdev

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
-import logo from './logo.jpg';
-import './App.css';
+import LikeButton from "./LikeButton";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Help me
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I survived
-        </a>
-      </header>
-    </div>
-  );
+import pfp from "./ressources/img/pfp.jpg"
+import throne from "./ressources/img/Throne.svg"
+import twitter from "./ressources/img/Twitter.svg"
+
+
+function App(){
+    return(
+        <div className="App">
+            <div className="Container">
+                <div className="SmallContainer">
+                    <div className="column">
+                        <img src={pfp} className="pfp" alt="logo"/>
+                    </div>
+                    <div className="column">
+                        <h1 className="Titlename">claymorwan</h1>
+                    </div>
+                </div>
+                <div className="SmallContainer">
+                    <div className="column">
+                        <ul>
+                            <a href="https://x.com/claymorwan">
+                                <div className="Link" >
+                                    <img src={twitter} className="LinkLogo" alt="Twitter-logo"/>
+                                    <span> Twitter</span>
+                                </div>
+                            </a>
+                        </ul>
+                    </div>
+                    <div className="column">
+                        <ul>
+                            <a href="https://throne.com/claymorwan">
+                                <div className="Link">
+                                    <img src={throne} className="LinkLogo" alt="Throne-logo"/>
+                                    <span> Throne</span>
+                                </div>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+            </footer>
+        </div>
+    );
 }
+
 
 export default App;
